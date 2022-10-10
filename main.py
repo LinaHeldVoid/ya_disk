2c8ae6f9b34b13a94a97a352f20729f0630dcfaf
Lin Amber <iltep118666@gmail.com>
2022-10-10 11:51:22 +0400

    1

---
+++ main.py
@@ -0,0 +1,18 @@
+import requests
+
+TOKEN = 'y0_AgAAAAAucQV2AADLWwAAAADOAh3zzZzkpHG8SEOsmedJo7vXHLAqO7I'   #ТОКЕН!!!
+PREPAREUPLOADURL = 'https://cloud-api.yandex.net/v1/disk/resources/upload'
+params = {'path': '/1.txt', 'overwrite': 'true'}
+headers = {'Accept': 'application/json', 'Authorization': TOKEN}
+
+file_path = '1.txt'
+
+response = requests.get(PREPAREUPLOADURL, params=params, headers=headers)
+print(response.text)
+
+puturl = response.json().get('href')
+print(puturl)
+
+files = {'file': open(file_path, 'rb')}
+response = requests.put(puturl, files=files, headers=headers)
+print(response)
